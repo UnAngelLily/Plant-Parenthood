8ee82c90a932e5dcb1623b6fd75ad571dd64370f
Lily <apple@apples-mbp.lan>
2020-09-20 15:33:12 -0600

    added haworthia information in the seed

--- models/seed.js
+++ models/seed.js
@@ -1,8 +1,280 @@
 module.exports = [
-  {
-  // seed
+  { // seed
+    name: Haworthia,
+    botanicalname: Haworthia Obtusa,
+    familyname: Asphodelaceae,
+
+    currentimage: ,
+
+    origin: South Africa (Eastern Cape),
+    expectedgrowth: 3in/6in,
+    leafshape: Rosette ,
+    color: green with transparent window-like petals,
+    // bloom: ,
+    bloomigseason: summer, monocot-blooms multiple times, white,
+
+    soiltype: quick draining porours pumice, aquarium gravel or perlite. Avoid sand mixed soil as it clogs up pores,
+    humidity: low,
+    temperatuer: 60-85 F ,
+    light: low indoor light or filtered/partial sun. 100-1000 Lux
+    water: deep infrequent watering with soil is bone dry. Plant is dormant in the winter and therefore requires less watering,
+    pottype: terracotta,
+    repotting: every 2-3 years ,
+    propgation: when new offsets grow from clumps around the base they can be left to develeop into a dense clump or pulled off and transplanted,
+    commonissues: root rot fron overwatering and sunburns from too much light,
+
+    dateacquired: January 25th 2019,
+    storeacquired: Denver Colorado - Nursery,
+    firstimage: ,
+
+    lastwatered: August 19th 2020,
+    lastrepotting: July 2020,
+    entrydate: September 20th 2020,
+    updateimage: ,
+    entry: Plant appears to be doing well. Offsets  are in a container next to the original plant. Due to it's stretch the mother plant is being held up with assistance by being tied to a stick to hoist it up. Plant tends to bow when unassisted. There still appears to be a nat problem with the plant as it may have root rot from overwatering so a moisture meter will determine when the soil is dry enough for it\'s next water cycle. ,
+
   },
-  {
-  // seed
+  {// seed
+    name: ,
+    botanicalname: ,
+    familyname: ,
+
+    currentimage: ,
+
+    origin: ,
+    expectedgrowth: ,
+    leafshape: ,
+    color: ,
+    // bloom: ,
+    bloomigseason: ,
+
+    soiltype: ,
+    humidity: ,
+    temperatuer: ,
+    light: ,
+    water: ,
+    pottype: ,
+    repotting: ,
+    propogation: ,
+    commonissues: ,
+
+    dateacquired: ,
+    storeacquired: ,
+    firstimage: ,
+
+    lastwatered: ,
+    lastrepotting: ,
+    entrydate: ,
+    updateimage: ,
+    entry: ,
+
+  },
+  { // seed
+    name: ,
+    botanicalname: ,
+    familyname: ,
+
+    currentimage: ,
+
+    origin: ,
+    expectedgrowth: ,
+    leafshape: ,
+    color: ,
+    // bloom: ,
+    bloomigseason: ,
+
+    soiltype: ,
+    humidity: ,
+    temperatuer: ,
+    light: ,
+    water: ,
+    pottype: ,
+    repotting: ,
+    commonissues: ,
+
+    dateacquired: ,
+    storeacquired: ,
+    firstimage: ,
+
+    lastwatered: ,
+    lastrepotting: ,
+    entrydate: ,
+    updateimage: ,
+    entry: ,
+
+  },
+  { // seed
+    name: ,
+    botanicalname: ,
+    familyname: ,
+
+    currentimage: ,
+
+    origin: ,
+    expectedgrowth: ,
+    leafshape: ,
+    color: ,
+    // bloom: ,
+    bloomigseason: ,
+
+    soiltype: ,
+    humidity: ,
+    temperatuer: ,
+    light: ,
+    water: ,
+    pottype: ,
+    repotting: ,
+    commonissues: ,
+
+    dateacquired: ,
+    storeacquired: ,
+    firstimage: ,
+
+    lastwatered: ,
+    lastrepotting: ,
+    entrydate: ,
+    updateimage: ,
+    entry: ,
+
+  },
+  { // seed
+    name: ,
+    botanicalname: ,
+    familyname: ,
+
+    currentimage: ,
+
+    origin: ,
+    expectedgrowth: ,
+    leafshape: ,
+    color: ,
+    // bloom: ,
+    bloomigseason: ,
+
+    soiltype: ,
+    humidity: ,
+    temperatuer: ,
+    light: ,
+    water: ,
+    pottype: ,
+    repotting: ,
+    propogation: ,
+    commonissues: ,
+
+    dateacquired: ,
+    storeacquired: ,
+    firstimage: ,
+
+    lastwatered: ,
+    lastrepotting: ,
+    entrydate: ,
+    updateimage: ,
+    entry: ,
+
+  },
+  { // seed
+    name: ,
+    botanicalname: ,
+    familyname: ,
+
+    currentimage: ,
+
+    origin: ,
+    expectedgrowth: ,
+    leafshape: ,
+    color: ,
+    // bloom: ,
+    bloomigseason: ,
+
+    soiltype: ,
+    humidity: ,
+    temperatuer: ,
+    light: ,
+    water: ,
+    pottype: ,
+    repotting: ,
+    propgation: ,
+    commonissues: ,
+
+    dateacquired: ,
+    storeacquired: ,
+    firstimage: ,
+
+    lastwatered: ,
+    lastrepotting: ,
+    entrydate: ,
+    updateimage: ,
+    entry: ,
+
+  },
+  { // seed
+    name: ,
+    botanicalname: ,
+    familyname: ,
+
+    currentimage: ,
+
+    origin: ,
+    expectedgrowth: ,
+    leafshape: ,
+    color: ,
+    // bloom: ,
+    bloomigseason: ,
+
+    soiltype: ,
+    humidity: ,
+    temperatuer: ,
+    light: ,
+    water: ,
+    pottype: ,
+    repotting: ,
+    propogation: ,
+    commonissues: ,
+
+    dateacquired: ,
+    storeacquired: ,
+    firstimage: ,
+
+    lastwatered: ,
+    lastrepotting: ,
+    entrydate: ,
+    updateimage: ,
+    entry: ,
+
+  },
+  { // seed
+    name: ,
+    botanicalname: ,
+    familyname: ,
+
+    currentimage: ,
+
+    origin: ,
+    expectedgrowth: ,
+    leafshape: ,
+    color: ,
+    // bloom: ,
+    bloomigseason: ,
+
+    soiltype: ,
+    humidity: ,
+    temperatuer: ,
+    light: ,
+    water: ,
+    pottype: ,
+    repotting: ,
+    propogation: ,
+    commonissues: ,
+
+    dateacquired: ,
+    storeacquired: ,
+    firstimage: ,
+
+    lastwatered: ,
+    lastrepotting: ,
+    entrydate: ,
+    updateimage: ,
+    entry: ,
+
   }
   ]
